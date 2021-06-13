07f6130c619c32b048d8e6c026d8a6ef60a96608
everedx <carlos.serranoe1992@gmail.com>
2021-06-13 11:02:06 +1000

    Manhole doesnt kill you now

--- Assets/Scripts/Misc/Manhole.cs
+++ Assets/Scripts/Misc/Manhole.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 
 public class Manhole : MonoBehaviour
 {
-    Animator animator;
+    [SerializeField] float timeInTheHole = 3;
 
+    Animator animator;
+    GameObject playerObject;
+    bool active;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        active = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.name == "Player") {
-            animator.SetBool("IsTriggered", true);
+        if (col.gameObject.name == "Player" && active) {
+            playerObject = col.gameObject;
+            animator.SetTrigger("IsTriggered");
 
             // TODO: Death screen? And definitely falling animation
             foreach (var rope in FindObjectsOfType<Rope>())
             {
                 rope.PlayerPoint = transform;
             }
-            
-            Destroy(col.gameObject);
+            playerObject.SetActive(false);
+            // Destroy(col.gameObject);
+            Invoke("ActivatePlayer", timeInTheHole);
+            Invoke("Reactivate", timeInTheHole*2);
+            active = false;
         }
+    }
+
+    private void ActivatePlayer()
+    {
+        animator.SetTrigger("IsTriggered");
+        playerObject.SetActive(true);
+
+    }
+
+    private void Reactivate()
+    {
+        active = true;
+
     }
 }
